5243380ff037939a5e06d62dd782b00f961fee11
rifat-mahmudul <rifatmahmudul.dev@gmail.com>
2025-12-29 10:16:10 +0600

    states done

--- src/app/(dashboard)/_components/states.tsx
+++ src/app/(dashboard)/_components/states.tsx
@@ -1,7 +1,57 @@
+"use client";
+import { Skeleton } from "@/components/ui/skeleton";
+import { useQuery } from "@tanstack/react-query";
+import { useSession } from "next-auth/react";
 import Image from "next/image";
 import React from "react";
 
 const States = () => {
+  const session = useSession();
+  const status = session?.status;
+  const token = session?.data?.user?.accessToken;
+
+  const { data = {}, isLoading } = useQuery({
+    queryKey: ["dashboard-overview"],
+    queryFn: async () => {
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_BACKEND_URL}/dashboard`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      const data = await res.json();
+      return data?.data;
+    },
+    enabled: !!token,
+  });
+
+  const LoadingStates = () => (
+    <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+      {[1, 2, 3].map((item) => (
+        <div
+          key={item}
+          className="bg-white p-6 shadow-[1px_2px_10px_0px_#00000029] rounded-lg"
+        >
+          <div className="flex items-center justify-between mb-6">
+            <Skeleton className="h-5 w-32" />
+            <Skeleton className="h-12 w-12 rounded-lg" />
+          </div>
+          <div className="mt-4">
+            <Skeleton className="h-10 w-20" />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+
+  if (isLoading || status == "loading") {
+    return <LoadingStates />;
+  }
+
   return (
     <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
       <div className="bg-white p-5 shadow-[1px_2px_10px_0px_#00000029] rounded-lg">
@@ -17,7 +67,9 @@ const States = () => {
         </div>
 
         <div>
-          <h1 className="text-4xl font-bold mt-5">14</h1>
+          <h1 className="text-4xl font-bold mt-5">
+            {data?.sessionsProcessedToday || "0"}
+          </h1>
         </div>
       </div>
 
@@ -34,7 +86,9 @@ const States = () => {
         </div>
 
         <div>
-          <h1 className="text-4xl font-bold mt-5">14%</h1>
+          <h1 className="text-4xl font-bold mt-5">
+            {data?.averageAlignmentScore || "0"}%
+          </h1>
         </div>
       </div>
 
@@ -51,7 +105,9 @@ const States = () => {
         </div>
 
         <div>
-          <h1 className="text-4xl font-bold mt-5">14</h1>
+          <h1 className="text-4xl font-bold mt-5">
+            {data?.highRiskFlagsIdentified || "0"}
+          </h1>
         </div>
       </div>
     </div>
