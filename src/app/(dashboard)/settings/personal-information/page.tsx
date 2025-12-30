b31cf9be2ffc926cf4e8f0cad7cb71aabae9211a
rifat-mahmudul <rifatmahmudul.dev@gmail.com>
2025-12-30 09:59:38 +0600

    settings done

---
+++ src/app/(dashboard)/settings/personal-information/page.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import SettingSidebar from "../_components/settings-sidebar";
+import PersonalInformationForm from "./_components/personal-information-form";
+import AppTopBar from "@/components/shared/app-topbar";
+
+const PersonalInfoPage = () => {
+  return (
+    <div className="">
+      <AppTopBar title="Settings" desc="Ready to compete in your profile?" />
+
+      <div className="grid grid-cols-1 md:grid-cols-7 gap-6 p-5">
+        <div className="md:col-span-2">
+          <SettingSidebar />
+        </div>
+        <div className="h-auto md:col-span-5">
+          <PersonalInformationForm />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PersonalInfoPage;
