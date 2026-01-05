fd33e6e890fccd981080ef86356a534bd7467ea7
rifat-mahmudul <rifatmahmudul.dev@gmail.com>
2026-01-05 18:57:08 +0600

    create invoie design done

--- src/app/(dashboard)/billing/_components/create-invoice.tsx
+++ src/app/(dashboard)/billing/_components/create-invoice.tsx
@@ -3,7 +3,6 @@ import {
   Dialog,
   DialogClose,
   DialogContent,
-  DialogDescription,
   DialogFooter,
   DialogHeader,
   DialogTitle,
@@ -21,28 +20,24 @@ export function CreateInvoice({ open, onOpenChange }: Props) {
     <Dialog open={open} onOpenChange={onOpenChange}>
       <form>
         <DialogContent className="sm:max-w-[425px]">
-          <DialogHeader>
-            <DialogTitle>Edit profile</DialogTitle>
-            <DialogDescription>
-              Make changes to your profile here. Click save when you&apos;re
-              done.
-            </DialogDescription>
+          <DialogHeader className="text-center">
+            <DialogTitle>Create Invoice</DialogTitle>
           </DialogHeader>
           <div className="grid gap-4">
             <div className="grid gap-3">
-              <Label htmlFor="name-1">Name</Label>
-              <Input id="name-1" name="name" defaultValue="Pedro Duarte" />
-            </div>
-            <div className="grid gap-3">
-              <Label htmlFor="username-1">Username</Label>
-              <Input id="username-1" name="username" defaultValue="@peduarte" />
+              <Label htmlFor="name-1">Enter Amount</Label>
+              <Input
+                name="name"
+                placeholder="Enter Amount"
+                className="h-[45px]"
+              />
             </div>
           </div>
           <DialogFooter>
             <DialogClose asChild>
               <Button variant="outline">Cancel</Button>
             </DialogClose>
-            <Button type="submit">Save changes</Button>
+            <Button type="submit">Create</Button>
           </DialogFooter>
         </DialogContent>
       </form>
